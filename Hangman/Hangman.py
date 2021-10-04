83284c556bf17de818902fb9e5610f9c5bedf3fe
Дмитрий Кочуров <d.m.kochurov@student.khai.edu>
2021-10-04 08:56:28 +0300

    Hangman Stage 1-3

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -2,10 +2,11 @@ import random
 
 print("HANGMAN")
 qw1 = ["python", "java", "javascript", "php"]
-qw2 = random.choise(qw1)
+qw2 = random.choice(qw1)
+qw3 = list(qw2)
 print(qw2)
-qw0 = input("Guess the word")
-if qw0 == qw2
+qw0 = input("Guess the word: ")
+if qw0 == qw2:
     print("You survived!")
 else:
     print("You lose!")
