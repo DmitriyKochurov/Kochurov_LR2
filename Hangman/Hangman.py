0c19f12ab4f15bd3bd5b4e0b35442c6fb893b308
Дмитрий Кочуров <d.m.kochurov@student.khai.edu>
2021-10-11 11:27:41 +0300

    Hangman Stage 8

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,41 +1,60 @@
 import random
 
-print("""HANGMAN
-Try guess word:""")
-
 q1 = ["python", "java", "javascript", "php"]
 q2 = random.choice(q1)
+q3 = set(q2)
+try_let = []
+wrong = []
+
+
+def word():
+    for letter in q2:
+        if letter in try_let:
+            print(letter, end='')
+        else:
+            print('_', end='')
 
 
 def hang():
-    let = ''
     tries = 8
     while tries > 0:
-        misses = 0
-        for letter in q2:
-            if letter in let:
-                print(letter, end=' ')
-            else:
-                print('_', end=' ')
-                misses = misses + 1
-        guess = input('')
-        if guess.capitalize() == guess and len(guess) == 1:
+        letter = input("\n")
+        if letter in try_let:
+            print("You've already guessed this letter!")
+            print("")
+            word()
+            continue
+        for i in q2:
+            if letter == i:
+                try_let.append(letter)
+        if letter.capitalize() == letter and len(letter) == 1:
             print("Please enter a lowercase English letter.")
             print("")
-        if len(guess) == 0 or len(guess) >= 2:
+            word()
+            continue
+        if len(letter) == 0 or len(letter) >= 2:
             print("You should input a single letter.")
-
-        if misses == 0:
-            print("You survived!")
-            break
-
-        guess = input('')
-        let += guess
-        if guess not in q2:
+            word()
+            continue
+        if letter not in q2:
+            wrong.append(letter)
             tries -= 1
             print("That letter doesn't appear in the word")
             if tries == 0:
                 print("You lost!")
+        print("")
+        word()
+        if q3 == set(try_let):
+            print(" You survived!")
+            break
 
 
-hang()
+while True:
+    answer = input('Type "play" to play the game, "exit" to quit: ')
+    if answer == "play":
+        print("Try guess the word: ")
+        print(len(q2) * '_', end='')
+        print("")
+        hang()
+    elif answer == "exit":
+        break
