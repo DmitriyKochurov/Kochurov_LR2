11fc320ea88989888b9b4a6cf14d82fb9b0bb695
Дмитрий Кочуров <d.m.kochurov@student.khai.edu>
2021-10-11 10:00:41 +0300

    Hangman Stage 6

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -3,14 +3,14 @@ import random
 print("""HANGMAN
 Try guess word:""")
 
-qw1 = ["python", "java", "javascript", "php"]
-qw2 = random.choice(qw1)
+q1 = ["python", "java", "javascript", "php"]
+q2 = random.choice(q1)
 let = ''
 tries = 8
 
 while tries > 0:
     misses = 0
-    for letter in qw2:
+    for letter in q2:
         if letter in let:
             print(letter,end=' ')
         else:
@@ -24,7 +24,8 @@ while tries > 0:
 
     guess = input('')
     let += guess
-    if guess not in qw2:
+    if guess not in q2:
         tries -= 1
+        print("That letter doesn't appear in the word")
         if tries == 0:
             print("You lost!")
