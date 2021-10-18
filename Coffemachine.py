4c02a0b92b67d28a595ed6f030a435d1f9cfa130
Дмитрий Кочуров <d.m.kochurov@student.khai.edu>
2021-10-18 08:33:41 +0300

    Coffemachine Stage 2-nd

--- Coffemachine.py
+++ Coffemachine.py
@@ -1,7 +1,22 @@
+water = 400
+milk = 200
+coffee = 100
+
+
+def coffe():
+    amount = int(input("Write how many cups you need: "))
+    print(f"""For {amount} cups of coffee you need:
+    {200 * amount} ml of water
+    {50 * amount} ml of milk
+    {15 * amount} g of coffee beans""")
+
+
+
+coffe()
 print("Starting to make a coffee")
 print("Grinding coffee beans")
 print("Boiling water")
 print("Mixing boiled water with crushed coffee beans")
 print("Pouring coffee into the cup")
 print("Pouring some milk into the cup")
-print("Coffe is ready!")
+print("Coffee is ready!")
