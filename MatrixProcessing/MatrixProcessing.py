0637ee29bae3f7e7dbe95cf452aac9014182fb5c
Дмитрий Кочуров <d.m.kochurov@student.khai.edu>
2021-11-21 18:24:28 +0200

    MatrixProcessing 2-nd Stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -1,45 +1,26 @@
+import constant as constant
+
 while True:
-    while True:
-        limns_1 = list(input("Please, input line and columns matrix A: \n"))
-        column_1 = int(limns_1[0])
-        line_1 = int(limns_1[2])
-        matrix_1 = []
-        line_true_a = 0
-        for i in range(column_1):
-            matrix_1.append(list(map(int, input().split())))
-        for height in range(len(matrix_1)):
-            if len(matrix_1[height]) == line_1:
-                line_true_a += 1
-        if line_true_a == column_1:
-            break
-        else:
-            print("Please, try again")
-
-    while True:
-        limns_2 = list(input("Please, input line and columns matrix B: \n"))
-        column_2 = int(limns_2[0])
-        line_2 = int(limns_2[2])
-        matrix_2 = []
-        line_true_b = 0
-        for i in range(column_2):
-            matrix_2.append(list(map(int, input().split())))
-        for height in range(len(matrix_2)):
-            if len(matrix_2[height]) == line_2:
-                line_true_b += 1
-        if line_true_b == column_2:
-            break
-        else:
-            print("Please, try again")
-
-    if column_1 == column_2 and line_1 == line_2:
+    limns_a1 = list(input("Please, input line and columns matrix A: \n"))
+    column_a1 = int(limns_a1[0])
+    line_a1 = int(limns_a1[2])
+    matx_a1 = []
+    line_true_a = 0
+    for i in range(column_a1):
+        matx_a1.append(list(map(int, input().split())))
+    for height in range(len(matx_a1)):
+        if len(matx_a1[height]) == line_a1:
+            line_true_a += 1
+    if line_true_a == column_a1:
+        constant = int(input())
+        break
+    else:
+        print("Please, try again")
         result = []
-        for i in range(column_1):
-            result.append([0]*line_2)
-        for i in range(len(matrix_1)):
-            for j in range(len(matrix_1[0])):
-                result[i][j] = matrix_1[i][j] + matrix_2[i][j]
+        for i in range(column_a1):
+            result.append([0] * line_a1)
+        for i in range(len(matx_a1)):
+            for j in range(len(matx_a1[0])):
+                result[i][j] = matx_a1[i][j] * constant
         for matrix_result in range(len(result)):
             print(*result[matrix_result])
-            break
-    else:
-        print("ERROR")
