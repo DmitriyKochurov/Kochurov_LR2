e211ba8de5f728c9e1d14be0876524bfd7e5edc2
Дмитрий Кочуров <d.m.kochurov@student.khai.edu>
2021-11-21 18:28:23 +0200

    MatrixProcessing 3-rd Stage

--- MatrixProcessing/MatrixProcessing.py
+++ MatrixProcessing/MatrixProcessing.py
@@ -1,26 +1,101 @@
-import constant as constant
+import ast
+
+def matx_input(row):
+    line = row
+    matx = []
+    for i in range(line):
+        t = []
+        for j in input().split():
+            if j != " ":
+                t.append(ast.literal_eval(j))
+        matx.append(t)
+    return matx
+
+
+def matrix_print(result, row, column):
+    line, columns = row, column
+    for i in range(line):
+        for j in range(columns):
+            print(result[i][j], end=' ')
+        print()
+
+
+def matx_sum(m1, m2, row, col):
+    line, columns = row, col
+    append = []
+    for i in range(line):
+        temp = []
+        for j in range(columns):
+            temp.append(m1[i][j] + m2[i][j])
+        append.append(temp)
+    return append
+
+
+def matx_number(m1, number_m):
+    multiply = []
+    for i in range(len(a)):
+        temp = []
+        for j in range(len(a[0])):
+            temp.append(float(m1[i][j]) * float(number_m))
+        multiply.append(temp)
+    return multiply
+
+
+def matx_element(l1, l2):
+    result = 0
+    for i in range(len(l1)):
+        result += l1[i] * l2[i]
+    return result
+
+
+def matx_two(m1, m2):
+    value = [[0 for row in range(len(m2[0]))] for column in range(len(m1))]
+    print(value)
+    for i in range(len(m1)):
+        l1 = m1[i]
+        for j in range(len(m2[0])):
+            l2 = [m2[x][j] for x in range(len(m2))]
+            value_range = matx_element(l1, l2)
+            value[i][j] = value_range
+    return value
+
 
 while True:
-    limns_a1 = list(input("Please, input line and columns matrix A: \n"))
-    column_a1 = int(limns_a1[0])
-    line_a1 = int(limns_a1[2])
-    matx_a1 = []
-    line_true_a = 0
-    for i in range(column_a1):
-        matx_a1.append(list(map(int, input().split())))
-    for height in range(len(matx_a1)):
-        if len(matx_a1[height]) == line_a1:
-            line_true_a += 1
-    if line_true_a == column_a1:
-        constant = int(input())
+    print('''1. Add matrices
+    2. Multiply matrix by a constant
+    3. Multiply matrices
+    0. Exit
+    ''', end='')
+    choice = int(input('Your choice:'))
+    if choice == 1:
+        m, n = map(int, input('Enter size of first matrix:').split())
+        print('Enter first matrix:')
+        a = matx_input(m)
+        p, q = map(int, input('Enter size of second matrix:').split())
+        print('Enter second matrix:')
+        b = matx_input(p)
+        if m != p and n != q:
+            print('The operation cannot be performed.')
+        else:
+            c = matx_sum(a, b, m, n)
+            matrix_print(c, m, n)
+    elif choice == 2:
+        m, n = map(int, input('Enter size of matrix:').split())
+        print('Enter matrix:')
+        a = matx_input(m)
+        number = float(input('Enter constant:'))
+        c = matx_number(a, number)
+        print('The result is:')
+        matrix_print(c, m, n)
+    elif choice == 3:
+        m, n = map(int, input('Enter size of first matrix:').split())
+        print('Enter first matrix:')
+        a = matx_input(m)
+        p, q = map(int, input('Enter size of second matrix:').split())
+        print('Enter second matrix:')
+        b = matx_input(p)
+        c = matx_two(a, b)
+        print('The result is:')
+        matrix_print(c, m, q)
+    elif choice == 0:
         break
-    else:
-        print("Please, try again")
-        result = []
-        for i in range(column_a1):
-            result.append([0] * line_a1)
-        for i in range(len(matx_a1)):
-            for j in range(len(matx_a1[0])):
-                result[i][j] = matx_a1[i][j] * constant
-        for matrix_result in range(len(result)):
-            print(*result[matrix_result])
